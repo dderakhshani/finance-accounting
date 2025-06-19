f0e8084c56bbdc9e21c275d3a074e709942251d5
Saeid Adabi <saeid.adabi1995@outlook.com>
2025-06-19 14:39:23 +0330

    change program file for sale

--- Backend/Microservices/Sale/Eefa.Sale.WebApi/Program.cs
+++ Backend/Microservices/Sale/Eefa.Sale.WebApi/Program.cs
@@ -1,25 +1,6 @@
+using Eefa.Sale.Infrastructure.Data.Context;
+using SharedCode;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+SharedProgram.SetupRun<SaleUnitOfWork>(builder, builder.Configuration);
