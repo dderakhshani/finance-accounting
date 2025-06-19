ed7bd6ad841ce07a3d02e249277a72ccd495bc79
Saeid Adabi <saeid.adabi1995@outlook.com>
2025-06-19 09:09:40 +0330

    راه اندازی اولیه پروژه 2

---
+++ Backend/Microservices/Warehouse/Eefa.Warehouse.Infrastructure/Data/Context/IWarehouseUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using Eefa.Commodity.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Eefa.Warehouse.Infrastructure.Data.Context
+{
+    public interface IWarehouseUnitOfWork
+    {
+        public  DbSet<Warehous> Warehouses { get; set; }
+
+        public  DbSet<WarehouseLayout> WarehouseLayouts { get; set; }
+    }
+}
---
+++ Backend/Microservices/Warehouse/Eefa.Warehouse.WebApi/Eefa.Warehouse.WebApi/Controllers/WarehouseBaseControlle.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Eefa.Common.Web;
+
+namespace Eefa.Warehouse.WebApi.Controllers
+{
+    [Route("api/warehouse/[controller]/[action]")]
+    [ApiController]
+    public class WarehouseBaseControlle : ApiControllerBase
+    {
+    }
+}
